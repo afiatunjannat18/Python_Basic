17b14babcd4495a61409917689bd53e51939c635
afiatunjannat18 <afiatunjannat18@gmail.com>
2026-09-06 15:54:26 -0400

    Add files via upload

---
+++ Conditionals.py
@@ -0,0 +1,18 @@
+rain = 10
+if rain == 1:
+    print("Ajke school e jabona")
+else:
+    print("Ajke school e jabo")
+
+
+#ekta number pos, neg or 0
+a=10
+
+if a > 0:
+    print("positive number")
+
+elif a < 0:
+    print("negetive number")
+else:
+    print("zero")
+    
