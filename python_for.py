9d94959c8ad462fec7a3268dd535083b3b3c2fe7
afiatunjannat18 <afiatunjannat18@gmail.com>
2026-09-10 03:16:25 -0400

    Add files via upload

---
+++ python_for.py
@@ -0,0 +1,14 @@
+#1-1000
+
+a=[1,2,3,4]
+b="Hello"
+for item in a:
+    print(a)
+    print(item) 
+    print(b)
+
+for i in range(1,1001):
+    print(i)
+
+for i in range(101):
+    print(f"Nasa is hacking..{i}%")
